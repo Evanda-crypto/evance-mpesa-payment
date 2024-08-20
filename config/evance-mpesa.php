5d46e1f001800adb8c36241891d9c7b641a91ba8
Evanda-crypto <evanceodhiambo07@gmail.com>
2024-08-20 14:45:34 +0300

    Feat:Add MPESA functionalities and use Laravel HTTP instead of php curl

--- config/evance-mpesa.php
+++ config/evance-mpesa.php
@@ -1,57 +1,68 @@
 <?php
 
-/*
- * Set Your configurations from .env
- */
 return [
 
     //Specify the environment mpesa is running, sandbox or production
-    'mpesa_env' => env('MPESA_ENV'),
-   /*-----------------------------------------
+    'mpesa_env' => env('MPESA_ENV', 'sandbox'),
+    /*-----------------------------------------
    |The App consumer key
    |------------------------------------------
    */
-   'consumer_key'   => env('CONSUMER_KEY'),
+    'consumer_key'   => env('CONSUMER_KEY'),
 
-   /*-----------------------------------------
+    /*-----------------------------------------
    |The App consumer Secret
    |------------------------------------------
    */
-   'consumer_secret' => env('CONSUMER_SECRET'),
+    'consumer_secret' => env('CONSUMER_SECRET'),
 
-   /*-----------------------------------------
+    /*-----------------------------------------
    |The paybill number
    |------------------------------------------
    */
-   'paybill'         => env('PAYBILL'),
+    'paybill' => env('PAYBILL'),
 
-   /*-----------------------------------------
+    /*-----------------------------------------
    |Lipa Na Mpesa Online Shortcode
    |------------------------------------------
    */
-   'shortcode'  => env("SHORTCODE"),
+    'shortcode'  => env("SHORTCODE"),
 
-   /*-----------------------------------------
+    /*-----------------------------------------
    |Lipa Na Mpesa Online Passkey
    |------------------------------------------
    */
-   'passkey' => env('PASSKEY'),
+    'passkey' => env('PASSKEY'),
 
     /*-----------------------------------------
    |C2B  Validation url
    |------------------------------------------
    */
-   'c2b_validate_callback' => env('C2B_VALIDATE_CALLBACK'),
+    'c2b_validate_callback' => env('C2B_VALIDATE_CALLBACK', url('/') . '/api/evance-mpesa/c2b_validate'),
 
-   /*-----------------------------------------
+    /*-----------------------------------------
    |C2B confirmation url
    |------------------------------------------
    */
-   'c2b_confirm_callback' => env('C2B_CONFIRM_CALLBACK'),
+    'c2b_confirm_callback' => env('C2B_CONFIRM_CALLBACK', url('/') . '/api/evance-mpesa/c2b_validate'),
 
-      /*-----------------------------------------
+    /*-----------------------------------------
    |C2B Callback url
    |------------------------------------------
    */
-  'callbackurl' => env('CALLBACK_URL'),
+    'c2b_callbackurl' => env('C2B_CALLBACK_URL', url('/') . '/api/evance-mpesa/c2b_callback'),
+
+    /*-----------------------------------------
+   |B2C Callback url
+   |------------------------------------------
+   */
+    'b2c_callbackurl' => env('B2C_CALLBACK_URL', url('/') . '/api/evance-mpesa/b2c_callback'),
+
+    /*-----------------------------------------
+   |B2C Queue Timeout url
+   |------------------------------------------
+   */
+
+    'queue_timeout_url' => env('QUEUE_TIMEOUT_URL', url('/') . '/api/evance-mpesa/queue_timeout_url')
+
 ];
